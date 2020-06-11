6d775d6fcc4fc9be03d55ff76eccb5e69e60ca16
Luis Fraguada <luis@mcneel.com>
2020-06-11 11:46:04 +0200

    serving example for testing

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 app.use(compression());
+app.use(express.static('example'))
 
 app.use('/', indexRouter);
 app.use('/definition', definitionRouter);

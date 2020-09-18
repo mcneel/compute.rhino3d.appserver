2ed96d1f4c471e4179fe2dcf32808d283aff7517
Luis Fraguada <fraguada@gmail.com>
2020-09-18 10:31:07 +0200

    reorg cache logic - in progress

--- routes/solve.js
+++ routes/solve.js
@@ -2,20 +2,64 @@ const express = require('express')
 const router = express.Router()
 const compute = require('compute-rhino3d')
 const {performance} = require('perf_hooks')
+
 const NodeCache = require('node-cache')
-var memjs = require('memjs')
-var mc = memjs.Client.create(process.env.MEMCACHIER_SERVERS, {
+const cache = new NodeCache()
+
+const memjs = require('memjs')
+const mc = memjs.Client.create(process.env.MEMCACHIER_SERVERS, {
   failover: true,  // default: false
   timeout: 1,      // default: 0.5 (seconds)
   keepAlive: true  // default: false
 })
 
-const cache = new NodeCache()
-
 function computeParams (req, res, next){
   compute.url = req.app.get('computeUrl')
   compute.authToken = process.env.COMPUTE_TOKEN
   compute.apiKey = process.env.RHINO_COMPUTE_KEY
+  next()
+}
+
+function collectParams (req, res, next){
+  res.locals.params = {}
+  switch (req.method){
+  case 'HEAD':
+  case 'GET':
+    res.locals.params.definition = req.params.definition
+    res.locals.params.inputs = req.query
+    break
+  case 'POST':
+    res.locals.params = req.body
+    break
+  default:
+    next()
+    break
+  }
+
+  next()
+
+}
+
+function checkCache (req, res, next){
+
+  res.locals.cacheKey = JSON.stringify(res.locals.params)
+
+  console.log('cachekey: ' + res.locals.cacheKey)
+
+  console.log('memcasher server: ' + process.env.MEMCACHIER_SERVERS)
+  if(process.env.MEMCACHIER_SERVERS === undefined){
+    // use node cache
+    res.locals.cacheResult = cache.get(res.locals.cacheKey)
+    next()
+  } else {
+    // use memcached
+    mc.get(res.locals.cacheKey, function(err, val) {
+      if(err == null && val != null) {
+        res.locals.cacheResult = val
+        next()
+      }
+    })
+  }
   next()
 }
 
@@ -29,111 +73,91 @@ function computeParams (req, res, next){
 function commonSolve (req, res, next){
   const timePostStart = performance.now()
 
-  let params = {}
-
-  switch (req.method){
-  case 'HEAD':
-  case 'GET':
-    params.definition = req.params.definition
-    params.inputs = req.query
-    break
-  case 'POST':
-    params = req.body
-    break
-  default:
-    next()
-    break
-  }
-
   // set general headers
   // what is the proper max-age, 31536000 = 1 year, 86400 = 1 day
   res.setHeader('Cache-Control', 'public, max-age=31536000')
   res.setHeader('Content-Type', 'application/json')
 
-  const cacheKey = JSON.stringify(params)
-  //let cachedResult = cache.get(cacheKey)
-  mc.get(cacheKey, function(err, val) {
-    if(err == null && val != null) { 
-      console.log('hit memcache')
-      const timespanPost = Math.round(performance.now() - timePostStart)
-      res.setHeader('Server-Timing', `cacheHit;dur=${timespanPost}`)
-      res.send(val)
-      return
-    } else {
+  console.log('cacheResult ' + res.locals.cacheResult)
 
-      let definition = req.app.get('definitions').find(o => o.name === params.definition)
-      if(!definition)
-        throw new Error('Definition not found on server.') 
-
-      // set parameters
-      let trees = []
-      if(params.inputs !== undefined) { // handle no inputs
-        for (let [key, value] of Object.entries(params.inputs)) {
-          let param = new compute.Grasshopper.DataTree('RH_IN:'+key)
-          param.append([0], [value])
-          trees.push(param)
-        }
-      }
-
-      let fullUrl = req.protocol + '://' + req.get('host')
-      let definitionPath = `${fullUrl}/definition/${definition.id}`
-      const timePreComputeServerCall = performance.now()
-      let computeServerTiming = null
-
-      // call compute server
-      compute.Grasshopper.evaluateDefinition(definitionPath, trees, false)
-        .then(computeResponse => {
-          computeServerTiming = computeResponse.headers
-          computeResponse.text().then(result=> {
-            const timeComputeServerCallComplete = performance.now()
-
-            let computeTimings = computeServerTiming.get('server-timing')
-            let sum = 0
-            computeTimings.split(',').forEach(element => {
-              let t = element.split('=')[1].trim()
-              sum += Number(t)
-            })
-            const timespanCompute = timeComputeServerCallComplete - timePreComputeServerCall
-            const timespanComputeNetwork = Math.round(timespanCompute - sum)
-            const timespanSetup = Math.round(timePreComputeServerCall - timePostStart)
-            const timing = `setup;dur=${timespanSetup}, ${computeTimings}, network;dur=${timespanComputeNetwork}`
-            
-            cache.set(cacheKey, result)
-            mc.set(cacheKey, result, {expires:0}, function(err, val){
-              console.log(err)
-              console.log(val)
-            })
-
-            res.setHeader('Server-Timing', timing)
-            res.send(result)
-          }).catch( (error) => { 
-            console.log(error)
-            res.send('error in solve')
-          })
-        })
-
-    }
-  })
-  /*
-  if (cachedResult) {
+  if(res.locals.cacheResult !== undefined) {
     const timespanPost = Math.round(performance.now() - timePostStart)
     res.setHeader('Server-Timing', `cacheHit;dur=${timespanPost}`)
-    res.send(cachedResult)
+    res.send(res.locals.cacheResult)
     return
+  } else {
+    //solve
+    let definition = req.app.get('definitions').find(o => o.name === res.locals.params.definition)
+    if(!definition)
+      throw new Error('Definition not found on server.')
+
+    // set parameters
+    let trees = []
+    if(res.locals.params.inputs !== undefined) { //TODO: handle no inputs
+      for (let [key, value] of Object.entries(res.locals.params.inputs)) {
+        let param = new compute.Grasshopper.DataTree('RH_IN:'+key)
+        param.append([0], [value])
+        trees.push(param)
+      }
+    }
+
+    let fullUrl = req.protocol + '://' + req.get('host')
+    let definitionPath = `${fullUrl}/definition/${definition.id}`
+    const timePreComputeServerCall = performance.now()
+    let computeServerTiming = null
+
+
+
+    // call compute server
+    compute.Grasshopper.evaluateDefinition(definitionPath, trees, false)
+      .then(computeResponse => {
+        computeServerTiming = computeResponse.headers
+        computeResponse.text().then(result=> {
+
+          console.log(result)
+          const timeComputeServerCallComplete = performance.now()
+
+          let computeTimings = computeServerTiming.get('server-timing')
+          let sum = 0
+          computeTimings.split(',').forEach(element => {
+            let t = element.split('=')[1].trim()
+            sum += Number(t)
+          })
+          const timespanCompute = timeComputeServerCallComplete - timePreComputeServerCall
+          const timespanComputeNetwork = Math.round(timespanCompute - sum)
+          const timespanSetup = Math.round(timePreComputeServerCall - timePostStart)
+          const timing = `setup;dur=${timespanSetup}, ${computeTimings}, network;dur=${timespanComputeNetwork}`
+          
+          cache.set(res.locals.cacheKey, result)
+          mc.set(res.locals.cacheKey, result, {expires:0}, function(err, val){
+            console.log(err)
+            console.log(val)
+          })
+
+          res.setHeader('Server-Timing', timing)
+          res.send(result)
+        }).catch( (error) => { 
+          console.log(error)
+          res.send('error in solve')
+        })
+      })
   }
-  */
-
-  
-
-  
-
-  
-  
 }
 
+/*
+if (cachedResult) {
+  const timespanPost = Math.round(performance.now() - timePostStart)
+  res.setHeader('Server-Timing', `cacheHit;dur=${timespanPost}`)
+  res.send(cachedResult)
+  return
+}
+*/
+
+const pipeline = [computeParams, collectParams, checkCache, commonSolve]
+
 // Handle different http methods
-router.head('/:definition',[computeParams,commonSolve]) // do we need this?
-router.get('/:definition', [computeParams,commonSolve])
-router.post('/', [computeParams,commonSolve])
+router.head('/:definition',pipeline) // do we need this?
+router.get('/:definition', pipeline)
+router.post('/', pipeline)
 
 module.exports = router

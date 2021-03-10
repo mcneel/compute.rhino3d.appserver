81bfb7a713d3e7891b09568ee906a74e78afa550
Will Pearson <will@mcneel.com>
2021-03-10 16:04:09 +0000

    Increase allowable request length
    When sending geometry as input, the default 1mb request limit isn't enough!

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express()
 if(process.env.NODE_ENV !== 'production')
   app.use(logger('dev'))
 
-app.use(express.json())
+app.use(express.json({limit: '10mb'}))
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 app.use(compression())
